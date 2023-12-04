de30b264467e6aa0ecc0fb1b205f53ea4b7093a9
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2023-12-04 12:45:23 +0530

    Update Header.js

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,8 +1,9 @@
+import logo from "../../public/assets/images/logo-light.svg";
 export default function Header() {
   return (
     <header className="hero">
       <nav>
-        <img src="../../public/assets/images/logo-light.svg" alt="Work it" />
+        <img src={logo} alt="Work it" />
         <a href="#">Apply for access</a>
       </nav>
       <h1>Data tailored to your needs.</h1>
